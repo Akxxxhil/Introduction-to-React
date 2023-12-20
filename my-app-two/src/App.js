c8a7eeea6993cc824ae50df76f234b7d80514bf1
Anshuman Biswal <101166423+Akxxxhil@users.noreply.github.com>
2023-12-20 16:15:22 +0530

    project 2

---
+++ my-app-two/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Input from './components/Input';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+   <div className='container'>
+   <Input/>
+   </div>
+    </>
+  );
+}
+
+export default App;
